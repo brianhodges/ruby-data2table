ebd5df41c1d2c8d79a7ce358791a9f7286e36a08
brianhodges <hodgesbn@yahoo.com>
2017-05-02 18:08:11 +0000

    initial code commit

---
+++ ruby-data2table.rb
@@ -0,0 +1,40 @@
+x = 0
+tab = ' ' * 4
+countries = [
+    {
+        :name => 'USA',
+		:population => '350 million'
+    },
+    {
+        :name => 'China',
+		:population => '1.4 billion'
+    },
+    {
+        :name => 'Russia',
+		:population => '145 million'
+    }
+]
+
+puts
+puts 'DUMP:'
+puts countries.inspect
+puts
+
+puts 'SANITIZED:'
+puts '['
+countries.each do |c|
+	puts tab + c.inspect
+end
+puts ']'
+
+puts
+# loop countries array of objects
+puts 'TABLE:'
+puts 'Countries | Population'
+puts '-' * 'Countries | Population'.size
+countries.each do |c|
+	puts c[:name] + 
+		(' ' * ('Countries '.size - c[:name].size)) + '|' +
+		c[:population]
+end
+puts
